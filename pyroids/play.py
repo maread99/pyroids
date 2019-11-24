afc653fa8be472ad817fa395f1f4ab1e3511c785
Marcus Read <marcusaread@gmail.com>
2019-11-24 17:32:32 +0000

    Module to launch application direct from command line

---
+++ pyroids/play.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+"""Launch application from the command line.
+
+    $ python -m pyroids.play
+
+Application settings can be optionally customised by passing the name of a 
+configuration file located in the pyroids.config directory. Example:
+
+    $ python -m pyroids.play novice
+
+See pyroids\config\template.py for instructions on setting up configuration 
+files.
+"""
+
+import sys
+import pyroids
+
+if __name__ == "__main__":
+    if len(sys.argv) is 2:
+        config_file = sys.argv[1]
+    else:
+        config_file = None
+    pyroids.launch(config_file)
